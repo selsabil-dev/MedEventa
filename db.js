d160c390dfe133167bffbc6b0690db7855d3f6a4
selsabil-dev <boukaousselsabil00@gmail.com>
2025-12-19 15:58:46 +0100

    s

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3307, // 3306 = défaut MySQL
+  port: process.env.DB_PORT, // 3307 dans ton .env
 });
 
 db.getConnection((err, connection) => {
